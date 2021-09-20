471633a4b16500e9d7bb5a3517d7213ade92c475
bassonby <bassonby@mail.ru>
2021-09-20 08:37:04 +0300

    20.09

---
+++ JS/lesson2/script2.js
@@ -0,0 +1,4 @@
+"use strict"
+const userName = prompt("введите ваше имя", "имя" );
+alert(`Теперь ваше имя - "${userName}"`)
+let question = confirm(`Ваше имя "${userName}"" введено верно?`);
---
+++ JS/lesson2/script3.js
@@ -0,0 +1,29 @@
+let lesson1 = 12.5;
+let lesson2 = 245;
+let lesson3 = "Hello";
+let lesson4 = undefined;
+let lesson5 = null;
+let lesson6 = true;
+let lesson7 = [1,5,7,12,3,"значение"];
+let lesson8 = {'Вася':'Имя'};
+let lesson9 = function() {};
+
+alert(typeof lesson1);
+alert(typeof lesson2);
+alert(typeof lesson3);
+alert(typeof lesson4);
+alert(typeof lesson5);
+alert(typeof lesson6);
+alert(typeof lesson7);
+alert(typeof lesson8);
+alert(typeof lesson9);
+
+console.log(typeof lesson1);
+console.log(typeof lesson2);
+console.log(typeof lesson3);
+console.log(typeof lesson4);
+console.log(typeof lesson5);
+console.log(typeof lesson6);
+console.log(typeof lesson7);
+console.log(typeof lesson8);
+console.log(typeof lesson9);
